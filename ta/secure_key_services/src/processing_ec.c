0c6b08d77566a5488c0600f625c4f5d507a55775
Ricardo Salveti <ricardo@foundries.io>
2019-04-30 20:34:40 -0300

    [FIO toup] sks ta: fix incorrect transient object release
    Transient objects should be released with TEE_FreeTransientObject.
    
    Signed-off-by: Ricardo Salveti <ricardo@foundries.io>

--- ta/secure_key_services/src/processing_ec.c
+++ ta/secure_key_services/src/processing_ec.c
@@ -1290,7 +1290,7 @@ uint32_t generate_ec_keys(struct sks_attribute_head *proc_params,
 
 bail:
 	if (tee_obj != TEE_HANDLE_NULL)
-		TEE_CloseObject(tee_obj);
+		TEE_FreeTransientObject(tee_obj);
 
 	return rv;
 }
