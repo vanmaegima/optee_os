f5e106fd88c599817b4d56e7c4c2c9ddbbc9e239
Rouven Czerwinski <r.czerwinski@pengutronix.de>
2020-02-05 11:10:03 +0100

    [FIO fromlist] caam: add funtion to increment PRIBLOB
    Add a function which increments the PRIBLOB field to 11. This way a HAB
    booted board can lock out generation of BLOBs for later boot stages.
    This will be used in the CAAM HUK patchset to prevent the normal world
    from generating the same HUK.
    
    Signed-off-by: Rouven Czerwinski <r.czerwinski@pengutronix.de>

--- core/drivers/crypto/caam/hal/common/hal_ctrl.c
+++ core/drivers/crypto/caam/hal/common/hal_ctrl.c
@@ -8,7 +8,10 @@
 #include <caam_hal_ctrl.h>
 #include <caam_io.h>
 #include <registers/ctrl_regs.h>
+#include <kernel/generic_boot.h>
 #include <registers/version_regs.h>
+#include <caam_hal_cfg.h>
+#include <mm/core_memprot.h>
 
 uint8_t caam_hal_ctrl_jrnum(vaddr_t baseaddr)
 {
@@ -37,4 +40,17 @@ uint8_t caam_hal_ctrl_hash_limit(vaddr_t baseaddr)
 	}
 
 	return UINT8_MAX;
+}
+
+void caam_hal_ctrl_inc_priblob(void){
+	vaddr_t ctrl_base = 0;
+	void *fdt = NULL;
+
+	fdt = get_dt();
+	if (fdt)
+		caam_hal_cfg_get_ctrl_dt(fdt, &ctrl_base);
+	if (!ctrl_base)
+		ctrl_base = (vaddr_t)phys_to_virt(CAAM_BASE, MEM_AREA_IO_SEC);
+
+	io_setbits32(ctrl_base + SCFGR, 0x3);
 }
