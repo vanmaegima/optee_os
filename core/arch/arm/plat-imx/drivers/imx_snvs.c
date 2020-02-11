5f2213dbc763cb5708e391fa64ab1b2d0c4d056d
Rouven Czerwinski <r.czerwinski@pengutronix.de>
2020-02-11 20:54:28 +0100

    [FIO fromlist] core: imx: add plat_rpmb_ready for i.MX6/7
    For i.MX6/7 RPMB key writes should only be allowed if the device is in
    the closed configuration and in the trusted or secure state.
    
    Signed-off-by: Rouven Czerwinski <r.czerwinski@pengutronix.de>

---
+++ core/arch/arm/plat-imx/drivers/imx_snvs.c
@@ -0,0 +1,17 @@
+// SPDX-License-Identifier: BSD-2-Clause
+#include <drivers/imx_snvs.h>
+#include <tee/tee_fs.h>
+
+bool plat_rpmb_ready(void)
+{
+	enum snvs_ssm_mode mode = SNVS_SSM_MODE_INIT;
+	enum snvs_security_cfg security = SNVS_SECURITY_CFG_OPEN;
+
+	mode = snvs_get_ssm_mode();
+	security = snvs_get_security_cfg();
+	if ((mode == SNVS_SSM_MODE_TRUSTED ||
+	     mode == SNVS_SSM_MODE_SECURE) &&
+	    security == SNVS_SECURITY_CFG_CLOSED)
+		return true;
+	return false;
+}
