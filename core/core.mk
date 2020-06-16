36742a82eab96c9319eb9bab2d6abb7a65b652ce
Jorge Ramirez-Ortiz <jorge@foundries.io>
2020-06-16 09:16:28 +0200

    [FIO toup] core: add support for libnxpse050
    Core work to support building the library
    
    Signed-off-by: Jorge Ramirez-Ortiz <jorge@foundries.io>
    Signed-off-by: Ricardo Salveti <ricardo@foundries.io>

--- core/core.mk
+++ core/core.mk
@@ -88,6 +88,22 @@ libname = utils
 libdir = lib/libutils
 include mk/lib.mk
 
+ifeq ($(CFG_CORE_SE05X), y)
+
+CFG_CORE_SE05X_I2C_BUS ?= 0x2
+CFG_CORE_SE05X_BAUDRATE ?= 3400000
+CFG_CORE_SE05X_INIT_NVM ?= 0
+
+$(call force, CFG_CRYPTO_RSASSA_NA1, n, not supported by se050)
+$(call force, CFG_NXP_SE05X_SVC, y)
+$(call force, CFG_NXP_SE05X_HMAC_DRV, y)
+$(call force, CFG_NXP_SE05X_RNG_DRV, y)
+
+libname = nxpse050
+libdir = lib/libnxpse050
+include mk/lib.mk
+endif
+
 # CFG_CRYPTOLIB_NAME must not be changed beyond this line
 CFG_CRYPTOLIB_NAME_$(CFG_CRYPTOLIB_NAME) := y
 
