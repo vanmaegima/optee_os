c05ffe7230743ee5a02f547909e6fc4344f545f0
Jorge Ramirez-Ortiz <jorge@foundries.io>
2020-08-25 22:34:01 +0200

    [FIO toup] drivers: rng_pta
    Add a Pseudo-TA to expose RNG from OPTEE.
    
    This uses a generic call to crypto_rng_read() to supply the
    random data.
    
    Signed-off-by: Jorge Ramirez-Ortiz <jorge@foundries.io>

--- core/drivers/sub.mk
+++ core/drivers/sub.mk
@@ -36,6 +36,8 @@ srcs-$(CFG_LS_GPIO) += ls_gpio.c
 srcs-$(CFG_LS_DSPI) += ls_dspi.c
 srcs-$(CFG_IMX_RNGB) += imx_rngb.c
 srcs-$(CFG_IMX_SC) += imx_mu.c
+srcs-$(CFG_IMX_OCOTP) += imx_ocotp.c
+srcs-$(CFG_RNG_PTA) += rng_pta.c
 
 subdirs-y += crypto
 subdirs-$(CFG_BNXT_FW) += bnxt
