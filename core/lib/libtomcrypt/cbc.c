db6c22a6bfe246c3dfde0ecc15227584807faaca
Jorge Ramirez-Ortiz <jorge@foundries.io>
2020-08-25 09:08:00 +0200

    [FIO toup] libtomcrypt: se050: pick and chose operations
    For performance reasons it is convenient to execute certain crypto
    operations in the processor rather than routing it to a secure element
    over i2c.
    
    Signed-off-by: Jorge Ramirez-Ortiz <jorge@foundries.io>

--- core/lib/libtomcrypt/cbc.c
+++ core/lib/libtomcrypt/cbc.c
@@ -120,7 +120,7 @@ static TEE_Result ltc_cbc_alloc_ctx(struct crypto_cipher_ctx **ctx_ret,
 	return TEE_SUCCESS;
 }
 
-#if defined(_CFG_CORE_LTC_AES)
+#if defined(_CFG_CORE_LTC_AES) && !defined(CFG_CORE_SE05X)
 TEE_Result crypto_aes_cbc_alloc_ctx(struct crypto_cipher_ctx **ctx)
 {
 	return ltc_cbc_alloc_ctx(ctx, find_cipher("aes"), false);
@@ -128,11 +128,12 @@ TEE_Result crypto_aes_cbc_alloc_ctx(struct crypto_cipher_ctx **ctx)
 #endif
 
 #if defined(_CFG_CORE_LTC_DES)
+#if !defined(CFG_CORE_SE05X)
 TEE_Result crypto_des_cbc_alloc_ctx(struct crypto_cipher_ctx **ctx)
 {
 	return ltc_cbc_alloc_ctx(ctx, find_cipher("des"), false);
 }
-
+#endif
 TEE_Result crypto_des3_cbc_alloc_ctx(struct crypto_cipher_ctx **ctx)
 {
 	return ltc_cbc_alloc_ctx(ctx, find_cipher("3des"), true);
