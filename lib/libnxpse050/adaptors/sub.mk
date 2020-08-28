9ca189b8624789045ef167969102dfbd788e9f33
Jorge Ramirez-Ortiz <jorge@foundries.io>
2020-08-28 16:21:49 +0200

    [FIO toup] libnxpse050: plug-and-trust github integration
    Update makefiles to interface to the plug-and-trust library
    
    Signed-off-by: Jorge Ramirez-Ortiz <jorge@foundries.io>

--- lib/libnxpse050/adaptors/sub.mk
+++ lib/libnxpse050/adaptors/sub.mk
@@ -9,15 +9,15 @@ cflags-y += -DSSS_USE_FTR_FILE
 
 incdirs-y += ./include
 incdirs-y += ../se050/glue/include
-incdirs-y += ../se050/simw-top/hostlib/hostLib/inc/
-incdirs-y += ../se050/simw-top/hostlib/hostLib/libCommon/infra/
-incdirs-y += ../se050/simw-top/hostlib/hostLib/libCommon/smCom/
-incdirs-y += ../se050/simw-top/hostlib/hostLib/libCommon/smCom/T1oI2C/
-incdirs-y += ../se050/simw-top/hostlib/hostLib/platform/inc/
-incdirs-y += ../se050/simw-top/hostlib/hostLib/se05x_03_xx_xx/
-incdirs-y += ../se050/simw-top/sss/inc/
-incdirs-y += ../se050/simw-top/sss/port/default/
-incdirs-y += ../se050/simw-top/sss/src/user/crypto/
+incdirs-y += ../se050/plug-and-trust/hostlib/hostLib/inc/
+incdirs-y += ../se050/plug-and-trust/hostlib/hostLib/libCommon/infra/
+incdirs-y += ../se050/plug-and-trust/hostlib/hostLib/libCommon/smCom/
+incdirs-y += ../se050/plug-and-trust/hostlib/hostLib/libCommon/smCom/T1oI2C/
+incdirs-y += ../se050/plug-and-trust/hostlib/hostLib/platform/inc/
+incdirs-y += ../se050/plug-and-trust/hostlib/hostLib/se05x_03_xx_xx/
+incdirs-y += ../se050/plug-and-trust/sss/inc/
+incdirs-y += ../se050/plug-and-trust/sss/port/default/
+incdirs-y += ../se050/plug-and-trust/sss/src/user/crypto/
 
 # called by the core library
 srcs-y += utils/scp_config.c
--- lib/libnxpse050/se050/sub.mk
+++ lib/libnxpse050/se050/sub.mk
@@ -10,16 +10,16 @@ cflags-y += -DT1oI2C
 cflags-y += -DSSS_USE_FTR_FILE
 
 incdirs-y += ./glue/include
-incdirs-y += ./simw-top/hostlib/hostLib/inc/
-incdirs-y += ./simw-top/hostlib/hostLib/libCommon/infra/
-incdirs-y += ./simw-top/hostlib/hostLib/libCommon/smCom/
-incdirs-y += ./simw-top/hostlib/hostLib/libCommon/smCom/T1oI2C/
-incdirs-y += ./simw-top/hostlib/hostLib/platform/inc/
-incdirs-y += ./simw-top/hostlib/hostLib/se05x_03_xx_xx/
-incdirs-y += ./simw-top/sss/inc/
-incdirs-y += ./simw-top/sss/ex/inc/
-incdirs-y += ./simw-top/sss/port/default/
-incdirs-y += ./simw-top/sss/src/user/crypto/
+incdirs-y += ./plug-and-trust/hostlib/hostLib/inc/
+incdirs-y += ./plug-and-trust/hostlib/hostLib/libCommon/infra/
+incdirs-y += ./plug-and-trust/hostlib/hostLib/libCommon/smCom/
+incdirs-y += ./plug-and-trust/hostlib/hostLib/libCommon/smCom/T1oI2C/
+incdirs-y += ./plug-and-trust/hostlib/hostLib/platform/inc/
+incdirs-y += ./plug-and-trust/hostlib/hostLib/se05x_03_xx_xx/
+incdirs-y += ./plug-and-trust/sss/inc/
+incdirs-y += ./plug-and-trust/sss/ex/inc/
+incdirs-y += ./plug-and-trust/sss/port/default/
+incdirs-y += ./plug-and-trust/sss/src/user/crypto/
 
 # glue code
 srcs-y += glue/stubs.c
@@ -28,42 +28,41 @@ srcs-y += glue/i2c.c
 srcs-y += glue/smCom.c
 
 # hostlib/hostLib/libCommon/smCom/
-srcs-y += simw-top/hostlib/hostLib/libCommon/smCom/smComT1oI2C.c
+srcs-y += plug-and-trust/hostlib/hostLib/libCommon/smCom/smComT1oI2C.c
 
 # hostlib/hostLib/libCommon/smCom/T1oI2C/
-srcs-y += simw-top/hostlib/hostLib/libCommon/smCom/T1oI2C/phNxpEse_Api.c
-srcs-y += simw-top/hostlib/hostLib/libCommon/smCom/T1oI2C/phNxpEseProto7816_3.c
+srcs-y += plug-and-trust/hostlib/hostLib/libCommon/smCom/T1oI2C/phNxpEse_Api.c
+srcs-y += plug-and-trust/hostlib/hostLib/libCommon/smCom/T1oI2C/phNxpEseProto7816_3.c
 
 # hostlib/hostLib/libCommon/infra/
-srcs-y += simw-top/hostlib/hostLib/libCommon/infra/sm_connect.c
-srcs-y += simw-top/hostlib/hostLib/libCommon/infra/sm_apdu.c
-srcs-y += simw-top/hostlib/hostLib/libCommon/infra/global_platf.c
+srcs-y += plug-and-trust/hostlib/hostLib/libCommon/infra/sm_connect.c
+srcs-y += plug-and-trust/hostlib/hostLib/libCommon/infra/sm_apdu.c
+srcs-y += plug-and-trust/hostlib/hostLib/libCommon/infra/global_platf.c
 
 # hostlib/hostLib/libCommon/nxScp/
-srcs-y += simw-top/hostlib/hostLib/libCommon/nxScp/nxScp03_Com.c
+srcs-y += plug-and-trust/hostlib/hostLib/libCommon/nxScp/nxScp03_Com.c
 
 # hostlib/hostLib/se05x_03_xx_xx/
-srcs-y += simw-top/hostlib/hostLib/se05x_03_xx_xx/se05x_APDU.c
+srcs-y += plug-and-trust/hostlib/hostLib/se05x_03_xx_xx/se05x_APDU.c
 
 # hostlib/hostLib/se05x/src
-srcs-y += simw-top/hostlib/hostLib/se05x/src/se05x_mw.c
-srcs-y += simw-top/hostlib/hostLib/se05x/src/se05x_tlv.c
-srcs-y += simw-top/hostlib/hostLib/se05x/src/se05x_ECC_curves.c
+srcs-y += plug-and-trust/hostlib/hostLib/se05x/src/se05x_mw.c
+srcs-y += plug-and-trust/hostlib/hostLib/se05x/src/se05x_tlv.c
+srcs-y += plug-and-trust/hostlib/hostLib/se05x/src/se05x_ECC_curves.c
 
 # sss/src
-srcs-y += simw-top/sss/src/fsl_sss_util_rsa_sign_utils.c
-srcs-y += simw-top/sss/src/fsl_sss_util_asn1_der.c
+srcs-y += plug-and-trust/sss/src/fsl_sss_util_rsa_sign_utils.c
+srcs-y += plug-and-trust/sss/src/fsl_sss_util_asn1_der.c
 
 # sss/src/se05x/
-srcs-y += simw-top/sss/src/se05x/fsl_sss_se05x_policy.c
-srcs-y += simw-top/sss/src/se05x/fsl_sss_se05x_mw.c
-srcs-y += simw-top/sss/src/se05x/fsl_sss_se05x_apis.c
-srcs-y += simw-top/sss/src/se05x/fsl_sss_se05x_scp03.c
+srcs-y += plug-and-trust/sss/src/se05x/fsl_sss_se05x_policy.c
+srcs-y += plug-and-trust/sss/src/se05x/fsl_sss_se05x_mw.c
+srcs-y += plug-and-trust/sss/src/se05x/fsl_sss_se05x_apis.c
+srcs-y += plug-and-trust/sss/src/se05x/fsl_sss_se05x_scp03.c
 
 # sss/src/user/
-srcs-y += simw-top/sss/src/user/fsl_sss_user_impl.c
+srcs-y += plug-and-trust/sss/src/user/fsl_sss_user_impl.c
 
 # sss/src/user/crypto
-srcs-y += simw-top/sss/src/user/crypto/aes.c
-srcs-y += simw-top/sss/src/user/crypto/aes_cmac.c
-srcs-y += simw-top/sss/src/user/crypto/aes_cmac_multistep.c
+srcs-y += plug-and-trust/sss/src/user/crypto/aes.c
+srcs-y += plug-and-trust/sss/src/user/crypto/aes_cmac.c
