ac5304edb5f1e4f07124c0e10d9da891ad9e5876
Ricardo Salveti <ricardo@foundries.io>
2020-10-08 19:58:48 -0300

    [FIO toup] plat: zynqmp: force disable core ALSR
    Disable core ASLR for two reasons:
    1. There is no source for ALSR seed, as trusted firmware
       does not provide DTB to OP-TEE.
    2. OP-TEE crashes during boot with enabled CFG_CORE_ASLR.
    
    Signed-off-by: Ricardo Salveti <ricardo@foundries.io>

--- core/arch/arm/plat-zynqmp/conf.mk
+++ core/arch/arm/plat-zynqmp/conf.mk
@@ -8,6 +8,12 @@ $(call force,CFG_GIC,y)
 $(call force,CFG_SECURE_TIME_SOURCE_CNTPCT,y)
 $(call force,CFG_WITH_ARM_TRUSTED_FW,y)
 
+# Disable core ASLR for two reasons:
+# 1. There is no source for ALSR seed, as trusted firmware
+#    does not provide DTB to OP-TEE.
+# 2. OP-TEE crashes during boot with enabled CFG_CORE_ASLR.
+$(call force,CFG_CORE_ASLR,n)
+
 ifeq ($(CFG_ARM64_core),y)
 $(call force,CFG_WITH_LPAE,y)
 else
